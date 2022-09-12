9b69837468a20a95ac9c95a652c7cd3fc5f0cf8a
Jean Viegas <jeanmelg@gmail.com.br>
2022-09-12 17:29:27 -0300

    feat: function to list unique and not empty industryes

--- src/insights.py
+++ src/insights.py
@@ -45,21 +45,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    jobs = read(path)
+    unique_industries = []
+    for job in jobs:
+        industry = job["industry"]
+        if industry not in unique_industries and industry != '':
+            unique_industries.append(industry)
+    return unique_industries
 
 
 def filter_by_industry(jobs, industry):
