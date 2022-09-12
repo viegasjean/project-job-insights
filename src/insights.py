771ff21d0999e78a78c2fee99ce0a1f73f7be591
Jean Viegas <jeanmelg@gmail.com.br>
2022-09-12 17:19:37 -0300

    feat: function to list unique jobs types

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,14 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+
+    jobs = read(path)
+    unique_job_types = []
+    for job in jobs:
+        job_type = job["job_type"]
+        if job_type not in unique_job_types:
+            unique_job_types.append(job_type)
+    return unique_job_types
 
 
 def filter_by_job_type(jobs, job_type):
