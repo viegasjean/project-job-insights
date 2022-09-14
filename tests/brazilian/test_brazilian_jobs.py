7dbc9c3d6b4cd9e3a6872db75b340691bc0495e8
Jean Viegas <jeanmelg@gmail.com.br>
2022-09-13 22:05:23 -0300

    feat: implements test to function brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,14 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = 'tests/mocks/brazilians_jobs.csv'
+
+    translated_jobs = read_brazilian_file(path)
+
+    assert type(translated_jobs) == list
+
+    for job in translated_jobs:
+        assert "title" in job
+        assert "salary" in job
+        assert "type" in job
