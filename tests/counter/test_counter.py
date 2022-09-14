a09d046b3eee46bb583908f286f28e321b109a98
Jean Viegas <jeanmelg@gmail.com.br>
2022-09-13 21:30:32 -0300

    feat: implements test to function test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = 'src/jobs.csv'
+    word = 'Python '
+    'Retorna um inteiro'
+    assert type(count_ocurrences(path, word)) == int
+    'Retorna a contagem 611'
+    assert count_ocurrences(path, word) == 611
